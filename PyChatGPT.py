7f662d5b89ca7d82bdd20e20cf5db7760d7d2e56
rquerk <example@mail.com>
2023-03-08 00:57:18 +0100

    i like spaces

--- PyChatGPT.py
+++ PyChatGPT.py
@@ -7,13 +7,13 @@ MODEL = "gpt-3.5-turbo"
 class ChatGPT:
     def __init__(self, api_key, role=ROLE):
         openai.api_key = api_key
-        self.dialog = [{"role":"system","content":role}]
+        self.dialog = [{"role": "system", "content": role}]
 
     def human_request(self, request):
-        self.dialog.append({"role":"user","content":request})
+        self.dialog.append({"role": "user", "content":request})
         gpt_response = openai.ChatCompletion.create(model=MODEL, messages=self.dialog)
 
         assistant_choice = gpt_response.choices[0].message.content
-        self.dialog.append({"role":"assistant","content":assistant_choice})
+        self.dialog.append({"role": "assistant", "content": assistant_choice})
 
         return assistant_choice
